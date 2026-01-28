25ae16f088efed8dcae40a49a56593ea3918d6b4
lyysax <lisa.anton0705@gmail.com>
2026-01-28 14:33:44 +0100

    hello world

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "templates/index.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
